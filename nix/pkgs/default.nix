120287bab402426d54fcddd9524b91fadcba265f
zeapoz <zeapo@pm.me>
2026-04-16 22:18:18 +0900

    fix(nix): add cargo outputHashes

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -1,6 +1,5 @@
 {
   perSystem = {
-    self',
     pkgs,
     env,
     meta,
@@ -15,6 +14,12 @@
 
       cargoLock = {
         inherit (meta) lockFile;
+        outputHashes = {
+          "halo2_gadgets-0.3.1" = "sha256-+1tMFB2w70mJPAiYJboO5rtu0C+rUuPi9qvHf7kk04U=";
+          "orchard-0.11.0" = "sha256-jVoOLFAQy1BtaOccQbb+dhrlcX0Jyqy65twuo0d0QlM=";
+          "sapling-crypto-0.5.0" = "sha256-UwxRBXEhvlcar4g3mKAirke3/oyFBI9+DHPVpLeb2bQ=";
+          "zair-core-0.1.0" = "sha256-68UcHWft8FFr1OQkq+K0gvhPvNcigs4APOjzVz7rlQI=";
+        };
       };
 
       nativeBuildInputs = meta.nativeBuildInputs ++ [meta.rustToolchain];
